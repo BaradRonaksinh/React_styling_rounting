54ad33282c051c237236aa401113f09f3f583fab
BaradRonaksinh <baradronaksinh21303@gmail.com>
2024-12-03 21:23:10 +0530

    some minor changed in Crud mui

--- src/Crud_Mui_Router/CrudMuiRoute.jsx
+++ src/Crud_Mui_Router/CrudMuiRoute.jsx
@@ -1,14 +1,54 @@
-import { Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import { Grid, Paper, Table, TableBody, IconButton,TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import EditIcon from "@mui/icons-material/Edit"
+import DeleteIcon from "@mui/icons-material/Delete"
 
 
 const CrudMuiRoute = () => {
-    const [prData, setPrData] = useState([])
+    // dev are think to this porstion are only for data segment...  *-*-*-*-*-*-*-*-*-*
+
+    // Single Product Data Show logic
+    const [prData, setPrData] = useState({
+        title:"",
+        price:"",
+        description:"",
+        category:"",
+        image:"",
+        
+    })
     useEffect(() => {
-        axios.get('https://fakestoreapi.com/products/')
-            .then(json => setPrData(json.data))
+        dispPrData()
     }, [])
+
+    // Display Data
+    const dispPrData = () => {
+        axios.get('https://fakestoreapi.com/products/')
+            .then(json => setAllPrData(json.data))
+    }
+
+    // All Product Data Show logic
+    const [allPrData,setAllPrData] = useState([])
+
+
+    // Edit Data
+    const editPrData = (id) => {
+        axios.get('https://fakestoreapi.com/products/' +id)
+            .then((res) => {
+                setPrData(res.data)
+            })
+        dispPrData()
+    }
+
+
+    // Handle Change EventFire
+    const prHandleChange = (e) => {
+        const {name , value} = e.target
+        setPrData({
+            ...prData,
+            [name]:value
+        })
+    }
 
     const rupeeFormatter = new Intl.NumberFormat('en-IN', {
         style: 'currency',
@@ -23,6 +63,7 @@ const CrudMuiRoute = () => {
                     <Table  sx={{ Width: 650 }} aria-label="simple table">
                         <TableHead>
                             <TableRow>
+                                <TableCell sx={{width:'100px',fontSize:'25px'}}>ID</TableCell>
                                 <TableCell sx={{width:'100px',fontSize:'25px'}}>Name</TableCell>
                                 <TableCell sx={{fontSize:'25px'}} align="center">Price</TableCell>
                                 <TableCell sx={{fontSize:'25px',width:'500px'}} align="center">Description</TableCell>
@@ -34,10 +75,16 @@ const CrudMuiRoute = () => {
                         </TableHead>
                         <TableBody>
                             {prData.map((i) => (
+                                
                                 <TableRow
-                                    key={i.title}
+                                    key={i.id}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 >
+                                    <TableCell component="th" scope="row" 
+                                    sx={{fontSize:'15px',
+                                    fontFamily:'inherit'}}>
+                                        {i.id}
+                                    </TableCell>
                                     <TableCell component="th" scope="row" 
                                     sx={{fontSize:'15px',
                                     fontFamily:'inherit'}}>
@@ -50,8 +97,18 @@ const CrudMuiRoute = () => {
                                     <TableCell align="left" ml={2}>{i.description}</TableCell>
                                     <TableCell align="center">{i.category}</TableCell>
                                     <TableCell align="center"><img src={i.image} alt="image" height={50} width={50} /></TableCell>
-                                    {/* <TableCell align="right">{i.rating.rate}</TableCell>
-                                    <TableCell align="right">{i.rating.count}</TableCell> */}
+                                    <TableCell align="right">
+                                        <IconButton 
+                                        onClick={() => editPrData(i.id)}
+                                        >
+                                            <EditIcon  color='primary'/>
+                                        </IconButton>
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <IconButton>
+                                            <DeleteIcon  color='warning'/>
+                                        </IconButton>
+                                    </TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
---
+++ src/React_styling/React_styling.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const React_styling = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default React_styling
